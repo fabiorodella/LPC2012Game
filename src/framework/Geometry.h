ab1c9b0b0a99cb53b3a782a0ae0c8f06e9d5a84e
Fabio Rodella <fabiorodella@gmail.com>
2012-07-03 10:27:52 -0300

    Geometry utilities and collision detection
    Geometry structs and helper functions (point, size and rect).
    Tile-based collision detection.

---
+++ src/framework/Geometry.h
@@ -0,0 +1,14 @@
+//
+//  Geometry.h
+//  LPC2012Game
+//
+//  Created by Fábio Rodella on 7/3/12.
+//  Copyright (c) 2012 Crocodella. All rights reserved.
+//
+
+#ifndef LPC2012Game_Geometry_h
+#define LPC2012Game_Geometry_h
+
+
+
+#endif
