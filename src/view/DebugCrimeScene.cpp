b0780c4747d9b3f3e2fa0cb175ae3e32e38b1c96
Fabio Rodella <fabiorodella@gmail.com>
2012-07-05 15:46:09 -0300

    More changes to the "murder mystery generator" algorithm

---
+++ src/view/DebugCrimeScene.cpp
@@ -0,0 +1,9 @@
+//
+//  DebugCrimeScene.cpp
+//  LPC2012Game
+//
+//  Created by Fábio Rodella on 7/5/12.
+//  Copyright (c) 2012 Crocodella. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/view/DebugCrimeScene.h
@@ -0,0 +1,14 @@
+//
+//  DebugCrimeScene.h
+//  LPC2012Game
+//
+//  Created by Fábio Rodella on 7/5/12.
+//  Copyright (c) 2012 Crocodella. All rights reserved.
+//
+
+#ifndef LPC2012Game_DebugCrimeScene_h
+#define LPC2012Game_DebugCrimeScene_h
+
+
+
+#endif
